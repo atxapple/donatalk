a7c605c84ebad6f4b898aa8cc567c1dfa8ed486d
Michael Lee <atxapplellc@gmail.com>
2025-04-20 23:55:43 -0500

    version removed

--- pages/api/create-payment-intent.ts
+++ pages/api/create-payment-intent.ts
@@ -1,9 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") return res.status(405).end("Method Not Allowed");
