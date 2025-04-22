c17289bb8c864c3b47f6c6957c9446b7bfa32b66
Michael <support@donatalk.com>
2025-04-22 14:58:27 -0700

    lint issue resolving

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,20 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-
+import { next } from 'eslint-config-next';
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  next(),
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off', // 👈 Add this line
+    },
+  },
 ]);
+
+
+
