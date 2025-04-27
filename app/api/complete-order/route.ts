428b50c573c4a814863891a8a6100d1b30836e65
Michael Lee <atxapplellc@gmail.com>
2025-04-27 03:42:29 -0500

    update fund function moved to lib

--- app/api/complete-order/route.ts
+++ app/api/complete-order/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { updateFunds } from '../update-funds/route';
+import { updateFunds } from '@/lib/updateFunds';
 
 export async function POST(req: Request) {
   try {
