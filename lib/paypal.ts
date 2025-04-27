c6fa31ab6fc7faf160ded07551fc5e5997f37d92
Michael Lee <atxapplellc@gmail.com>
2025-04-27 03:38:02 -0500

    removed unnecessary parts

--- lib/paypal.ts
+++
@@ -1,9 +0,0 @@
-// lib/paypal.ts
-import * as paypal from '@paypal/checkout-server-sdk';
-
-const environment = new paypal.core.LiveEnvironment(
-  process.env.PAYPAL_CLIENT_ID!,
-  process.env.PAYPAL_CLIENT_SECRET!
-);
-
-export const client = new paypal.core.PayPalHttpClient(environment);
