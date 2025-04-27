7dc685b4087d1f19a7aa933e546b10ab9494c6ac
Michael Lee <atxapplellc@gmail.com>
2025-04-26 20:58:53 -0500

    linting error resolving

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-import { next } from 'eslint-config-next';
+import nextConfig from 'eslint-config-next';
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
