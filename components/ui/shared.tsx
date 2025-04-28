a530affad6dea435183421ec49488b1029f70af8
Michael Lee <atxapplellc@gmail.com>
2025-04-27 23:52:50 -0500

    "subtitle color has been improved. more readable. listner sharable page has been created in app. Payment needs to be completed"

--- components/ui/shared.tsx
+++ components/ui/shared.tsx
@@ -16,7 +16,8 @@ export const Title = styled('h1', {
 export const Subtitle = styled('p', {
   textAlign: 'center',
   fontSize: '$base',
-  color: '$mediumgray',
+  // color: '$mediumgray',
+  color: '$darkgray',
   marginBottom: '$sm',
 });
 
